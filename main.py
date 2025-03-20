7948e8a9965c84db3e8d270aba708e5d01f89d34
Arminas03 <arminas.raz@gmail.com>
2025-03-20 20:06:38 +0100

    buy_hold_spy strategy

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import backtrader as bt
-from buy_hold_spy import BuyHoldSpy
-from import_data import add_data
+from buy_hold_spy import BuyHoldSpy, add_data
 
 
 def add_analyzers(cerebro: bt.Cerebro):
     cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name = "sharpe_ratio")
     cerebro.addanalyzer(bt.analyzers.Returns, _name = "returns")
+    cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, _name = "trade_analyzer")
 
 
-def print_analysis(run, cerebro):
+def print_analysis(run):
     print(f"Sharpe: {round(
         run[0].analyzers.sharpe_ratio.get_analysis()["sharperatio"], 4
     )}")
-    print(f"Net profit: {round(cerebro.broker.get_value() - 100_000, 2)}")
+    trade_analysis = run[0].analyzers.trade_analyzer.get_analysis()
+    print(f"Net profit: {round(trade_analysis.pnl.net.total, 2)}")
 
 
 def main():
     cerebro = bt.Cerebro()
     cerebro.addstrategy(BuyHoldSpy)
-    cerebro.broker.set_cash(100_000)
+    cerebro.broker.set_cash(10000)
 
     add_data(cerebro)
     add_analyzers(cerebro)
@@ -26,7 +27,7 @@ def main():
     run = cerebro.run()
 
     cerebro.plot()
-    print_analysis(run, cerebro)
+    print_analysis(run)
 
 
 if __name__ == "__main__":
